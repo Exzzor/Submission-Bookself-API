51ed6c0c5c1cd0adc80b09d553e2239aeeb8bbb0
Adi Nugraha <adinugrahaputra03@gmail.com>
2023-10-01 18:49:12 +0700

    Port 5005

--- src/notes.js
+++ src/notes.js
@@ -1,3 +1,18 @@
-const notes = [];
+const notes = [
+    {
+        id: "Maling",
+        title: "kolor",
+        createdAt: "adi",
+        updatedAt: "adi",
+        tags: ["ID","COK"],
+        body: "wwaduh",
+      
+    },
+    {
+        method:'GET',
+        path:'/notes{id}',
+        handler:() => {},
+    }
+];
 
-module.exports = notes;
+module.exports=notes;
